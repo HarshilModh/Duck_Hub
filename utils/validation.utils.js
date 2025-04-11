6595cc5a4e41582364fcb8d2f51fd561ea70b00d
Vamshi_3103 <vkovvuri@stevens.edu>
2025-04-11 02:50:21 -0400

    Wrote User Data Functions, installed mongoDB package

--- utils/validation.utils.js
+++ utils/validation.utils.js
@@ -1,8 +1,29 @@
+import { ObjectId } from "mongodb";
+
 export const isValidEmail = (email) => {
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return emailRegex.test(email);
-}
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+};
 export const isValidPassword = (password) => {
-    const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
-    return passwordRegex.test(password);
-}
+  const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+  return passwordRegex.test(password);
+};
+
+export const isValidID = (id, varName) => {
+  if (!id) {
+    throw new Error(`Error: You must provide a ${varName}`);
+  }
+  if (typeof id !== "string") {
+    throw new Error(`Error: ${varName} must be a string`);
+  }
+  id = id.trim();
+  if (id.length === 0) {
+    throw new Error(
+      `Error: ${varName} cannot be an empty string or just spaces`
+    );
+  }
+  if (!ObjectId.isValid(id)) {
+    throw new Error(`Error: ${varName} invalid object ID`);
+  }
+  return id;
+};
