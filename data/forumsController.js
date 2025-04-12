498cc2931b83b9824f6b791e8f377165d809a6e5
Modh Harshil Chetankumar <78226025+HarshilModh@users.noreply.github.com>
2025-04-11 21:38:33 -0400

    Course,Course review, Forum Controller created

---
+++ data/forumsController.js
@@ -0,0 +1,60 @@
+import mongoose from "mongoose";
+import Forum from "../models/forums.model.js";
+import { isValidID } from "../utils/validation.utils.js";
+
+// Create a new forum post
+export const createForumPost = async (req, res) => {
+};
+// Get all forum posts
+export const getAllForumPosts = async (req, res) => {
+};
+// Get a forum post by ID
+export const getForumPostById = async (req, res) => {
+};
+// Update a forum post by ID
+export const updateForumPostById = async (req, res) => {
+};
+// Delete a forum post by ID
+export const deleteForumPostById = async (req, res) => {
+};
+// Filter forum posts by keyword in title or content
+export const filterForumPosts = async (req, res) => {
+};
+// Get forum posts by user ID
+export const getForumPostsByUserId = async (req, res) => {
+};
+// Get forum posts by tag ID
+export const getForumPostsByTagId = async (req, res) => {
+};
+// Get forum posts by status
+export const getForumPostsByStatus = async (req, res) => {
+};
+// Get trending forum posts
+export const getTrendingForumPosts = async (req, res) => {
+};
+// Upvote a forum post
+export const upvoteForumPost = async (req, res) => {
+};
+// Downvote a forum post
+export const downvoteForumPost = async (req, res) => {
+};
+// Report a forum post
+export const reportForumPost = async (req, res) => {
+};
+// Unreport a forum post
+export const unreportForumPost = async (req, res) => {
+};
+// Get reported forum posts
+export const getReportedForumPosts = async (req, res) => {
+};
+// Change forum post status
+export const changeForumPostStatus = async (req, res) => {
+};
+// Get forum posts by multiple tags
+export const getForumPostsByMultipleTags = async (req, res) => {
+};
+// Delete a forum post image
+export const deleteForumPostImage = async (req, res) => {
+};
+
+
