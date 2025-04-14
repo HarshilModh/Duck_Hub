eac134e69af1dd31e211b7b128767eb85352932f
Vamshi_3103 <vkovvuri@stevens.edu>
2025-04-14 15:06:31 -0400

    Added Cloudinary files

---
+++ middlewares/cloudinary.js
@@ -0,0 +1,35 @@
+import dotenv from "dotenv";
+import { v2 as cloudinary } from "cloudinary";
+
+dotenv.config();
+
+cloudinary.config({
+  cloud_name: "dz1ld7w31",
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const url = async (public_Id) => {
+  return cloudinary.url(public_Id, {
+    transformation: [
+      {
+        fetch_format: "auto", // asking cloudinary to deliver the most efficient format for that browser or device. (Ex : jpeg to webp)
+        quality: "auto",
+      },
+      {
+        width: 1200,
+        height: 1200,
+        crop: "fill",
+        gravity: "auto",
+      },
+    ],
+  });
+};
+
+export const userImage = async (imagePath) => {
+  const results = await cloudinary.uploader.upload(imagePath);
+  const finalURL = await url(results.public_id);
+  console.log(finalURL);
+};
+
+userImage();
