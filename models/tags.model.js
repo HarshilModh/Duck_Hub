aa5c1cd92b320bf767c249fe8df7877d75e6c03c
David Kim <davidsunkim0710@gmail.com>
2025-04-16 15:09:16 -0400

    added models

---
+++ models/tags.model.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const tagsSchema = new mongoose.Schema(
+  {
+    _id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+      trim: true,
+    },
+    name: {
+      type: String,
+      trim: true,
+      lowercase: true,
+      required: true,
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+      trim: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    updatedAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  { timestamps: true }
+);
+const Tags = mongoose.model("Tags", tagsSchema);
+export default Tags;
