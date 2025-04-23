56c206cb1b5c4167365832a208d742300686d445
David Kim <davidsunkim0710@gmail.com>
2025-04-22 20:32:55 -0400

    Finished Tags Routes and CRUD

---
+++ data/tagController.js
@@ -0,0 +1,109 @@
+import Tag from "../models/tags.model.js";
+import { getUserById } from "./userController.js";
+import {
+  isValidID,
+  isValidArray,
+  isValidString,
+} from "../utils/validation.utils.js";
+
+export const createTag = async (userId, name) => {
+  name = name.trim();
+  // Check if the required fields are passed
+  if (!userId || !name) {
+    throw new Error("userId, and name are required.");
+  }
+  // Validate Tag Name
+  name = isValidString(name, "Tag Name");
+  // Validate userID
+  userId = isValidID(userId, "userId");
+  // Check for User
+  const user = await getUserById(userId);
+  if (!user) {
+    throw new Error("No User Found With Given ID");
+  }
+  // Check if Tag already exists
+  name = name.toLowerCase();
+  const isTagAlreadyExists = await Tag.findOne({ name });
+  if (isTagAlreadyExists) {
+    throw new Error("Tag already exists");
+  }
+  // Create new Tag
+  const newTag = new Tag({
+    userId,
+    name,
+  });
+  // Try Saving
+  try {
+    const savedTag = await newTag.save();
+    if (!savedTag || !savedTag._id) {
+      throw new Error("Could not create a new tag");
+    }
+    return savedTag;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
+
+export const getAllTags = async () => {
+  try {
+    const allTags = await Tag.find();
+    if (!allTags) {
+      throw new Error("No Tags yet");
+    }
+    return allTags;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
+
+export const getTagsById = async (id) => {
+  id = isValidID(id, "Tag ID");
+  const tag = await Tag.findById(id);
+  if (!tag) {
+    throw new Error("Tag not found");
+  }
+  return tag;
+};
+
+export const getTagsByUser = async (userId) => {
+  userId = isValidID(userId, "User ID");
+  try {
+    const tags = await Tag.find({ userId: userId });
+  } catch (error) {
+    throw new Error("Failed to fetch Tags: " + error.message);
+  }
+  if (!tags || tags.length === 0) {
+    throw new Error("Sorry, you haven't created any Tags yet!!");
+  }
+  return tags;
+};
+
+export const updateTagById = async (tagId, updatedTag) => {
+  try {
+    tagId = isValidID(tagId, "Tag ID");
+    const existingTag = await Tag.findById(tagId);
+    if (!existingTag) {
+      throw new Error("Could not find the tag with the given ID");
+    }
+    if (updatedTag.name) {
+      existingTag.name = isValidString(updatedTag.name, "tag Name");
+    }
+    const newTag = await existingTag.save();
+    return newTag;
+  } catch (error) {
+    throw new Error("Error Updating the tag:" + error.message);
+  }
+};
+
+export const deleteTagById = async (tagId) => {
+  try {
+    tagId = isValidID(tagId, "Tag ID");
+    const deletedTag = await Tag.findByIdAndDelete(tagId);
+    if (!deletedTag) {
+      throw new Error("Tag not found");
+    }
+    return { message: "Tag deleted successfully", deletedTag };
+  } catch (error) {
+    throw new Error("Error deleting the tag:" + error.message);
+  }
+};
---
+++ routes/tags.routes.js
@@ -0,0 +1,65 @@
+import express from "express";
+const router = express.Router();
+import {
+  createTag,
+  getAllTags,
+  getTagsById,
+  getTagsByUser,
+  updateTagById,
+  deleteTagById,
+} from "../data/tagController";
+
+router.route("/").post(async (req, res) => {
+  const { userId, name } = req.body;
+  try {
+    const createdTag = await createTag(userId, name);
+    return res.status(201).json(createdTag);
+  } catch (e) {
+    return res.status(500).json({ error: e });
+  }
+});
+router.route("/").get(async (req, res) => {
+  try {
+    const allTags = await getAllTags();
+    return res.status(201).json(allTags);
+  } catch (e) {
+    return res.status(500).json({ error: e });
+  }
+});
+router.route("/:id").get(async (req, res) => {
+  const tagId = req.params.id;
+  try {
+    const idTag = await getTagsById(tagId);
+    return res.status(201).json(idTag);
+  } catch (e) {
+    return res.status(500).json({ error: e });
+  }
+});
+router.route("/:userId").get(async (req, res) => {
+  const userId = req.params.id;
+  try {
+    const userTag = await getTagsById(userId);
+    return res.status(201).json(userTag);
+  } catch (e) {
+    return res.status(500).json({ error: e });
+  }
+});
+router.route("/update/:id").put(async (req, res) => {
+  const tagId = req.params.id;
+  const { newTag } = req.body;
+  try {
+    const updatedTag = await updateTagById(tagId, newTag);
+    return res.status(201).json(updatedTag);
+  } catch (e) {
+    return res.status(500).json({ error: e });
+  }
+});
+router.route("/delete/:id").delete(async (req, res) => {
+  const tagId = req.params.id;
+  try {
+    const deletedTag = await deleteTagById(tagId);
+    return res.status(201).json(deletedTag);
+  } catch (e) {
+    return res.status(500).json({ error: e });
+  }
+});
