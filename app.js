0799d6715b8689b5c1da394376d8bf822993cc4c
Modh Harshil Chetankumar <78226025+HarshilModh@users.noreply.github.com>
2025-06-18 16:36:18 -0400

    Changed port for deployment

--- app.js
+++ app.js
@@ -88,7 +88,10 @@ indexRoutes(app);
 
 
 //  Connect DB & start server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, '0.0.0.0', () => {
-  console.log(`✅ Server running on http://0.0.0.0:${PORT}`);
-});
+connectDB()
+  .then(() =>
+    app.listen(process.env.PORT, '0.0.0.0', () =>
+      console.log(`✅ Server running on http://0.0.0.0:${process.env.PORT}`)
+    )
+  )
+  .catch((err) => console.error("❌ DB connection error:", err));
